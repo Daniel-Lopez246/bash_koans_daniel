3329b10683161c11852422b7fb7e452659038967
Daniel Lopez <dlope121@asu.edu>
2025-10-09 17:59:28 -0700

    From Loops Bash koan, solved test_while_loops() function and added comments explaining each step

--- src/00_about_loops.sh
+++ src/00_about_loops.sh
@@ -14,11 +14,16 @@ test_for_loops() {
 }
 
 test_while_loops() {
+  # Declare a global variable named 'counter' and assigned 0
   counter=0
+  # The while loop runs as long as the condition inside [ ] is true
+  # Here, it continues while 'counter' is less than 5
   while [ $counter -lt 5 ]; do
+    # Increment 'counter' by 1 each iteration
     (( counter++ ))
   done
 
+  # When the loop ends, counter should be 5 (since it stops once counter is no longer < 5)
   assertEqual $counter 5
 }
 

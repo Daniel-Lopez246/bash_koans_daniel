5749da46b3fdebd86b741718f78f3e6cec61e7a2
Daniel Lopez <dlope121@asu.edu>
2025-10-09 18:18:06 -0700

    From Redirection Bash koan, solved test_redirecting_stderr_to_file() function and added comments explaining each step

--- src/00_about_redirection.sh
+++ src/00_about_redirection.sh
@@ -16,10 +16,16 @@ test_redirecting_stdout_to_file() {
 }
 
 test_redirecting_stderr_to_file() {
+  # '2>' redirects stderr to a file.
+  # The function 'output_stderr' just runs: printf "out to stderr" 1>&2
+  # '1>&2' redirects stdout (1) to the same destination as stderr (2).
+  # Therefore, stdout will also go into tmp/redirect_test3.txt.
   output_stderr 2> tmp/redirect_test2.txt
 
+  # Create local variable 'contents' and assign the contents of the file we just wrote to.
   local contents=$(cat tmp/redirect_test2.txt)
 
+  # The function should have written "out to stderr" to the file.
   assertEqual "$contents" "out to stderr"
 }
 

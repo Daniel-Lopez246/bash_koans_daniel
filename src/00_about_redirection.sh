6e6bc25ee16e2c06796d507db054bdf8fcc5c09b
Daniel Lopez <dlope121@asu.edu>
2025-10-09 18:24:06 -0700

    From Redirection Bash koan, solved test_redirecting_stdout_and_stderr_to_file() function and added comments explaining each step

--- src/00_about_redirection.sh
+++ src/00_about_redirection.sh
@@ -60,11 +60,18 @@ test_redirecting_stderr_to_stdout() {
 }
 
 test_redirecting_stdout_and_stderr_to_file() {
+  # The function 'output_both' runs: printf "out to stdout" and printf "out to stderr" 1>&2
+  # '1>' redirects stdout to tmp/redirect_test5.txt
+  # '2>' redirects stderr to tmp/redirect_test6.txt
+  # This keeps the two outputs separate.
   output_both 1> tmp/redirect_test5.txt 2> tmp/redirect_test6.txt
 
+  # Create local variable 'contents5' and assign the stdout contents of the file we just wrote to.
   local contents5=$(cat tmp/redirect_test5.txt)
+  # Create local variable 'contents6' and assign the stderr contents of the file we just wrote to.
   local contents6=$(cat tmp/redirect_test6.txt)
 
+  # Verify that stdout and stderr each went to the correct file.
   assertEqual "$contents5" "out to stdout"
   assertEqual "$contents6" "out to stderr"
 
